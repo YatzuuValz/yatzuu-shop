50f0baedffb570325d817802516f3de5af002dd5
YatzuuValz <yahya1muhandar@gmail.com>
2025-09-17 00:32:44 +0700

    selesai settup

--- main/views.py
+++ main/views.py
@@ -1,13 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import ProductForms
+from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
 
-# Create your views here.
 def show_main(request):
+    product_list = Product.objects.all()
+
     context = {
         'npm' : '2406415936',
         'name': 'Yahya Muhandar Fathana',
         'class': 'PBP F',
-        'ProjectName': 'Yatzuu Shop'
-
+        'ProjectName': 'Yatzuu Shop',
+        'product_list': product_list,
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForms(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
+
+def show_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    product.increment_views()
+
+    context = {
+        'product': product
+    }
+
+    return render(request, "product_detail.html", context)
+
+def show_xml(request):
+     news_list = Product.objects.all()
+     xml_data = serializers.serialize("xml", news_list)
+     return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):
+    news_list = Product.objects.all()
+    json_data = serializers.serialize("json", news_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+def show_xml_by_id(request, news_id):
+   try:
+       news_item = Product.objects.filter(pk=news_id)
+       xml_data = serializers.serialize("xml", news_item)
+       return HttpResponse(xml_data, content_type="application/xml")
+   except Product.DoesNotExist:
+       return HttpResponse(status=404)
+   
+
+def show_json_by_id(request, news_id):
+   try:
+       news_item = Product.objects.get(pk=news_id)
+       json_data = serializers.serialize("json", [news_item])
+       return HttpResponse(json_data, content_type="application/json")
+   except Product.DoesNotExist:
+       return HttpResponse(status=404)
