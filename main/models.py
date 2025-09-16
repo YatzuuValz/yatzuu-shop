698ddf0b96f86a0e74e0038b2945b8af6a4c3ae1
YatzuuValz <yahya1muhandar@gmail.com>
2025-09-17 02:01:52 +0700

    testingg

--- main/models.py
+++ main/models.py
@@ -12,14 +12,14 @@ class Product(models.Model):
         ('training', 'Training Equipment'),
         ('merch', 'Merchandise'),
     ]
-    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField(default=0)
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
     category = models.CharField(max_length=20,choices=CATEGORY_CHOICES,default='update')
     rating = models.IntegerField(default=0)
-
+    item_views = models.PositiveIntegerField(default=0)
     
     is_featured = models.BooleanField(default=False)
 
