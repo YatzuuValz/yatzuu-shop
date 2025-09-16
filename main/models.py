b29fa65563e69895289df671822491b0b3fb4955
YatzuuValz <yahya1muhandar@gmail.com>
2025-09-17 02:11:11 +0700

    tambah ongkir

--- main/models.py
+++ main/models.py
@@ -15,12 +15,12 @@ class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField(default=0)
+    ongkir = models.IntegerField(default=0)
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
     category = models.CharField(max_length=20,choices=CATEGORY_CHOICES,default='update')
     rating = models.IntegerField(default=0)
     item_views = models.PositiveIntegerField(default=0)
-    
     is_featured = models.BooleanField(default=False)
 
     
