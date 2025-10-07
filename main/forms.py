1d39e3de139d640eb18b1bf4a90861e58a4baf2b
YatzuuValz <yahya1muhandar@gmail.com>
2025-10-07 07:40:31 +0700

    demo t3

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
 from main.models import Product
+from main.Seller import Seller
 
 class ProductForms(ModelForm):
     class Meta:
         model = Product
         fields = ["name", "price", "description", "thumbnail","category","is_featured"]
+class SellerForms(ModelForm):
+    class Meta:
+        model = Seller
+        fields = ["Nama", "tanggallahir", "email", "notelp", "linksocmed","password"]
